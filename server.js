f92fde7e6bc091ddd2b2e315459aaba066aa00b2
ush189 <github@ush189.de>
2016-04-30 17:43:41 +0200

    added route for page subscription

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require('express');
+var request = require('request');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.send('Hello Facebook Messenger!');
 });
 
-app.get('/webhook', function (req, res) {
+app.get('/webhook', function(req, res) {
     if (req.query['hub.verify_token'] === 'verify_token_facebook_messenger') {
         res.send(req.query['hub.challenge']);
     } else {
@@ -15,6 +16,17 @@ app.get('/webhook', function (req, res) {
     }
 });
 
-app.listen(port, function () {
+app.get('/subscribe', function(req, res) {
+    var pageAccessToken = 'EAAIJf2PGBwIBACpZBwQNFi7QslU70YbQZAVIZCGUSnk0G8eI9ZBWMuIZCSnvYVvKKV0xviQ6QRmI1rHcFsbgZCcaZBNYZCKX1gjjZC1vnbFj9VCP7UMXHruWruqtWhB0y3wGLXnMwsR8bFCaZBDF5l9mmwcZAyYOVAsRDgzZC0fSyzBzFgZDZD';
+    request.post('https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=' + pageAccessToken, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send(body);
+        } else {
+            res.send('Error, wrong page access token');
+        }
+    });
+});
+
+app.listen(port, function() {
     console.log('Server listening at port %s', port);
 });
