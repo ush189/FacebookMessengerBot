237ce73a76f40057291e7e8c40c9a00524e36c73
ush189 <github@ush189.de>
2016-04-30 14:22:39 +0200

    added route for webhook

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ app.get('/', function (req, res) {
     res.send('Hello Facebook Messenger!');
 });
 
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.verify_token'] === 'verify_token_facebook_messenger') {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.send('Error, wrong validation token');
+    }
+});
+
 app.listen(port, function () {
     console.log('Server listening at port %s', port);
 });
